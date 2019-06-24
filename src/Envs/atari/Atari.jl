9e5ac16ca5ed3daa46a91330b18335abb55ed5fd
Kartikey Gupta <kartikeygupta9097@gmail.com>
2019-06-25 02:59:16 +0530

    core AtariEnv funcs complete

--- src/Envs/atari/Atari.jl
+++ src/Envs/atari/Atari.jl
@@ -1,8 +1,9 @@
+using .Space: Box, Discrete
 using ArcadeLearningEnvironment
 
 setLoggerMode!(:error)  # Only log errors
 
-include("atari_roms.jl")
+abstract type AbstractEnv end
 
 struct AtariEnv <: AbstractEnv
     game_path::String
@@ -10,22 +11,102 @@ struct AtariEnv <: AbstractEnv
     action_set::Array{UInt, 1}
     action_space::Discrete
     observation_space::Box
-    frameskip
+    frameskip::Union{Int, UnitRange{Int}}
+    obs_type::Symbol
 end
 
-function AtariEnv(game_path::String, obs_type::Symbol=:ram, frameskip=(2, 5), repeat_action_probability::AbstractFloat=0f0, full_action_space::Bool=false)
-    @assert isfile(game_path) "$game_path is not the path to a valid file "
+function AtariEnv(game_path::String, obs_type::Symbol=:ram, frameskip::Union{Int, UnitRange{Int}}=2:5, repeat_action_probability::AbstractFloat=0f0, full_action_space::Bool=false)
     @assert obs_type ∈ (:ram, :image) "Invalid observation type. Choose either :ram or :image"
     @assert isa(repeat_action_probability, AbstractFloat) || isa(repeat_action_probability, Integer) "Invalid repeat_action_probability"
 
     ale = ALE_new()
-    loadROM(ale, game_path)
+    loadROM(ale, game_path)  # This is will also check if the loaded file is valid or not.
     action_set = full_action_space ? getLegalActionSet(ale) : getMinimalActionSet(ale)
-    action_space = full_action_space ? getLegalActionSize(ale) : getMinimalActionSize(ale)
+    action_set .+= 1  # Converting to one indexed array
+    action_space = full_action_space ? Discrete(getLegalActionSize(ale)) : Discrete(getMinimalActionSize(ale))
 
     screen_width, screen_height = getScreenWidth(ale), getScreenHeight(ale)
 
     observation_space = obs_type==:ram ? Box(0, 255, (128,), UInt8) : Box(0, 255, (screen_height, screen_width, 3), UInt8)
 
-    AtariEnv(game_path, ale, action_set, action_space, observation_space, frameskip)
+    AtariEnv(game_path, ale, action_set, action_space, observation_space, frameskip, obs_type)
+end
+
+
+function step!(env::AtariEnv, action)
+    @assert action ∈ env.action_space "Action $action is invalid."
+    reward = 0f0
+
+    num_steps = isa(env.frameskip, UnitRange) ? rand(env.frameskip) : env.frameskip
+
+    for _ in 1:num_steps
+        reward += act(env.ale, action)
+    end
+    ob = _get_obs(env)
+    return ob, reward, game_over(env.ale), Dict(:ale_lives => lives(env.ale))
+end
+
+_get_obs(env::AtariEnv) = env.obs_type == :ram ? getRAM(env.ale) : getScreenRGB(env.ale)
+
+function reset!(env::AtariEnv)
+    reset_game(env.ale)
+    _get_obs(env)
+end
+
+
+function clonestate(env::AtariEnv)
+    state_ref = cloneState(ale)
+    state = encodeState(state_ref)
+    deleteState(state_ref)
+    return state
+end
+
+function restorestate(env::AtariEnv)
+    state_ref = decodeState(state)
+    restoreState(state_ref)
+    deleteState(state_ref)
+end
+
+
+ACTION_MEANING = [
+    (:NOOP,),
+    (:FIRE,),
+    (:UP,),
+    (:RIGHT,),
+    (:LEFT,),
+    (:DOWN,),
+    (:UP, :RIGHT),
+    (:UP, :LEFT),
+    (:DOWN, :RIGHT),
+    (:DOWN, :LEFT),
+    (:UP, :FIRE),
+    (:RIGHT, :FIRE),
+    (:LEFT, :FIRE),
+    (:DOWN, :FIRE),
+    (:UP, :RIGHT, :FIRE),
+    (:UP, :LEFT, :FIRE),
+    (:DOWN, :RIGHT, :FIRE),
+    (:DOWN, :LEFT, :FIRE)
+]
+
+getactionmeanings(env::AtariEnv) = [ACTION_MEANING[i] for i in env.action_set]
+
+
+function getkeystoaction(env::AtariEnv)
+    KEYWORD_TO_KEY = Dict(
+        :UP    => Int('w'),
+        :DOWN  => Int('s'),
+        :LEFT  => Int('a'),
+        :RIGHT => Int('d'),
+        :FIRE  => Int(' ')
+    )
+
+    keystoaction = Dict()
+
+    for (action_id, action_meaning) ∈ enumerate(getactionmeanings(env))
+        keys_ = [key for (keyword, key) ∈ collect(zip(keys(KEYWORD_TO_KEY), values(KEYWORD_TO_KEY)))
+                    if keyword ∈ action_meaning] |> sort |> Tuple
+        keystoaction[keys_] = action_id
+    end
+    keystoaction
 end
